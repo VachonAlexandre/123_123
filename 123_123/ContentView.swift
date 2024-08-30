6fbdbafb88f658d579530c54a4989c4f0c2ca001
VachonAlexandre <201367503@colegeahuntsic.qc.ca>
2024-08-30 12:46:51 -0400

    Initial Commit

---
+++ 123_123/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  123_123
+//
+//  Created by Alexandre Vachon (Étudiant) on 2024-08-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 123_123/_23_123App.swift
@@ -0,0 +1,17 @@
+//
+//  _23_123App.swift
+//  123_123
+//
+//  Created by Alexandre Vachon (Étudiant) on 2024-08-30.
+//
+
+import SwiftUI
+
+@main
+struct _23_123App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
